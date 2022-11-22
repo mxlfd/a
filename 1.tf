6b2883943e92047781d2ad73f258f7c8b714f964
Max <max@air3.local>
2022-11-22 09:40:23 +0100

    Variables

--- 1.tf
+++ 1.tf
@@ -8,18 +8,18 @@ terraform {
 }
 
 ####################################
-variable "aws_access_key" {
+variable "AWS_ACCESS_KEY_ID" {
   sensitive = true
 }
 
-variable "aws_secret_key" {
+variable "AWS_SECRET_ACCESS_KEY" {
   sensitive = true
 }
 
 ####################################
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = "us-west-2"
 }
 
